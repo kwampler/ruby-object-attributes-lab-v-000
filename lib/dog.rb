a1e33deee56b90cd5c7cf8fb3a7c4346cc8d6688
katejsenger <katejsenger@yahoo.com>
2018-10-05 23:06:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,5 @@
 class Dog
-  def initialize(name)
-    @name = name
+  def name = (dogs_name)
+    @this_dogs_name=this_dogs_name
   end
-  def name
-    @name
-  end
-  def name=(new_name)
-    @name = new_name
-  end
-  def name
-    "#{@name}".strip
-  end
-  def initialize(breed)
-    @breed = breed
-  end
-   def breed
-    @breed
-  end
-  def breed=(new_breed)
-    @breed = new_breed
-  end
-  def breed
-    "#{@breed}".strip
-  end  
-end 
-    
+fido = Dog.new
