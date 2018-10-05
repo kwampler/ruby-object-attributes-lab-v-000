b4b0d899d055976708dc4daa6405ad03aca55f17
katejsenger <katejsenger@yahoo.com>
2018-10-05 23:22:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,13 +3,12 @@ class Dog
     @name = name
     @breed = breed
   end
-  def name ()
+  def full_name (name, breed)
+    name, breed.split
     @name
+    @breed
   end
-  def name=(new_name)
-    @name = new_name
-  end
-  def name
+  def full_name=(new_name)
     "#{@name}".strip
   end
   def initialize(breed)
